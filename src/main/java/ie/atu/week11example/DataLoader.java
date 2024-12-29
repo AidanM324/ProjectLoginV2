1cbfc75b804d16d215bfd48e5647f27906cefb2b
Aidan Malone <g00423306@atu.ie>
2024-12-29 13:20:55 +0000

    fixed DataLoader

--- src/main/java/ie/atu/week11example/DataLoader.java
+++ src/main/java/ie/atu/week11example/DataLoader.java
@@ -16,7 +16,11 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
-        //Person testData = new Person(1L, "Aidan", 21, "aidan@atu.ie", "aidanm", "12345", "ireland");
-        //personRepository.save(testData);
+        Person testData = new Person(1L, "Aidan", 21, "aidan@atu.ie", "aidanm", "12345", "ireland");
+        Person testData2 = new Person(2L, "Paul", 41, "Paul@atu.ie", "Paull", "56789", "ireland");
+        Person testData3 = new Person(3L, "Jamie", 20, "jamie@atu.ie", "jamiem", "11223", "ireland");
+        personRepository.save(testData);
+        personRepository.save(testData2);
+        personRepository.save(testData3);
     }
 }
